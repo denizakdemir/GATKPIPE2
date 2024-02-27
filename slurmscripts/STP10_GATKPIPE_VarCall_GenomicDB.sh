5327120ae50b6c4e601374d55fe592bd9404f1e9
denizakdemir <deniz.akdemir.work@gmail.com>
2024-02-27 18:56:54 +0100

    STP10

--- slurmscripts/STP10_GATKPIPE_VarCall_GenomicDB.sh
+++ slurmscripts/STP10_GATKPIPE_VarCall_GenomicDB.sh
@@ -2,33 +2,29 @@
 #SBATCH --job-name=GenomicDB
 #SBATCH --output=GenomicDB_%j.log
 #SBATCH --error=GenomicDB_%j.err
-#SBATCH --partition=medium
+#SBATCH --partition=long
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --time=24:00:00
-#SBATCH --mem=128G
+#SBATCH --cpus-per-task=24
+#SBATCH --time=60:00:00
+#SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user="deniz.akdemir.work@gmail.com"
 
-# Load modules
+# Load GATK module
 module load GATK/4.1.2.0-GCCcore-8.2.0-Java-1.8 
 module load SAMtools/1.16.1-GCC-11.3.0
 
-# Relative paths from the script's current working directory
-REF_DIR="./0_index/referenceIPO323"
-REF_GENOME="$REF_DIR/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
+# Assuming BASE_DIR is your current working directory
+BASE_DIR="/home/deniz/GenomicData/github/GATKPIPE2"  # Update this path if needed
 
-GVCF_DIR="./4_processing/GVCF"
+REF_DIR="/home/deniz/GenomicData/github/GATKPIPE2/0_index/referenceIPO323"  # Adjusted to the detailed path
+REF_GENOME="$/home/deniz/GenomicData/github/GATKPIPE2/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
+
+GVCF_DIR="$/home/deniz/GenomicData/github/GATKPIPE2/4_processing/GVCF"
 DB_PATH="$GVCF_DIR/GenomicsDB"
 FINAL_VCF="$GVCF_DIR/final_joint_called.vcf"
 TMP_DIR="$GVCF_DIR/tmp"
-
-# Ensure the script is executed from the correct directory
-if [[ $PWD != */GATKPIPE2 ]]; then
-  echo "This script must be run from the /home/deniz/GenomicData/github/GATKPIPE2 directory"
-  exit 1
-fi
 
 # Index the reference genome using samtools
 samtools faidx $REF_GENOME
@@ -37,14 +33,15 @@ samtools faidx $REF_GENOME
 gatk CreateSequenceDictionary -R $REF_GENOME
 
 # Generate a BED file from the reference genome fasta index
-awk -v FS="\t" -v OFS="\t" '{print $1 FS "0" FS ($2-1)}' "${REF_GENOME}.fai" > "$GVCF_DIR/genome.bed"
+awk -v FS="\t" -v OFS="\t" '{print $1 FS "0" FS ($2-1)}' "${REF_GENOME}.fai" > "${GVCF_DIR}/genome.bed"
 
 # Update INTERVAL_LIST variable to point to the newly created interval list
-INTERVAL_LIST="$GVCF_DIR/genome.bed"
+INTERVAL_LIST="$GVCF_DIR/interval.list"
 NEW_INTERVAL_LIST="$GVCF_DIR/formatted.interval_list"
 
 # Reformat interval list
 awk '{print $1 ":" $2 "-" $3}' $INTERVAL_LIST > $NEW_INTERVAL_LIST
+
 
 # Create the temporary directory if it does not exist
 if [ ! -d "$TMP_DIR" ]; then
@@ -52,7 +49,7 @@ if [ ! -d "$TMP_DIR" ]; then
   chmod 777 "$TMP_DIR"
 fi
 
-GenomicsDBImport command
+# Continue with the GenomicsDBImport as before
 gatk GenomicsDBImport \
    --genomicsdb-workspace-path $DB_PATH \
    -L $NEW_INTERVAL_LIST \
