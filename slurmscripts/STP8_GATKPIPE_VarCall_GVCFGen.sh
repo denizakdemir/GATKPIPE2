c190670cf20757b6de232a8b046e7a77faf32fa2
denizakdemir <deniz.akdemir.work@gmail.com>
2024-04-25 12:34:14 +0200

    STP8

--- slurmscripts/STP8_GATKPIPE_VarCall_GVCFGen.sh
+++ slurmscripts/STP8_GATKPIPE_VarCall_GVCFGen.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=72:00:00
+#SBATCH --time=120:00:00
 #SBATCH --mem=128G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user="deniz.akdemir.work@gmail.com"
