f027b81c9cce6d36f4e7978a36011966240e1108
denizakdemir <deniz.akdemir.work@gmail.com>
2024-09-29 18:29:48 +0200

    STP 15

--- slurmscripts/STP15_GetSequencesAroundHits.sh
+++ slurmscripts/STP15_GetSequencesAroundHits.sh
@@ -17,17 +17,17 @@ module load BEDTools/2.28.0-GCC-8.2.0-2.31.1
 
 # Define variables
 REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_FILE="5_annotation/snpEff_snps/geno_filtered_snps.vcf"
+VCF_DIR="4_processing/GVCF"
 OUTPUT_DIR="extracted_sequences"
+OUTPUT_DIR_CONSENSUS="${OUTPUT_DIR}/consensus_sequences"
 mkdir -p ${OUTPUT_DIR}
-
+mkdir -p ${OUTPUT_DIR_CONSENSUS}
 
 # Define regions to extract - 500bp flanking regions
-REGIONS=("3:247434-251178")  # "7:2122486-2123486" "2:3164088-3165088")
+REGIONS=("3:247434-251178")
 
 # Extract SNP positions and create BED files
 for REGION in "${REGIONS[@]}"; do
-    # Extract the chromosome and position
     IFS=':' read -ra ADDR <<< "$REGION"
     CHROM=${ADDR[0]}
     IFS='-' read -ra POS <<< "${ADDR[1]}"
@@ -42,47 +42,6 @@ for REGION in "${REGIONS[@]}"; do
 done
 
 echo "Reference sequence extraction completed."
-# Define variables
-REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_DIR="4_processing/GVCF"
-OUTPUT_DIR="extracted_sequences"
-mkdir -p ${OUTPUT_DIR}
-
-# Define SNP positions
-SNP_POSITIONS=("3:247434-251178")
-
-# Iterate over individual VCF files
-for VCF_FILE in ${VCF_DIR}/S*.vcf.gz; do
-    # Extract the strain name from the VCF filename
-    STRAIN_NAME=$(basename ${VCF_FILE} | cut -d'.' -f1)
-    
-    # Make sure the VCF file is indexed
-    if [ ! -f "${VCF_FILE}.tbi" ]; then
-        bcftools index ${VCF_FILE}
-    fi
-    
-    # Extract genotypes for each SNP position for the current strain
-    for SNP_POS in "${SNP_POSITIONS[@]}"; do
-        # Define output file name based on strain and SNP position
-        OUTPUT_FILE="${OUTPUT_DIR}/${STRAIN_NAME}_$(echo ${SNP_POS} | tr ':' '_')_genotypes.txt"
-        
-        # Use bcftools query to extract genotypes
-        bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n' -r ${SNP_POS} ${VCF_FILE} > ${OUTPUT_FILE}
-    done
-done
-
-echo "Genotype extraction for individual strains completed."
-
-# Define directories and files
-REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_DIR="4_processing/GVCF"
-OUTPUT_DIR="extracted_sequences"
-OUTPUT_DIR_CONSENSUS="${OUTPUT_DIR}/consensus_sequences"
-mkdir -p ${OUTPUT_DIR}
-mkdir -p ${OUTPUT_DIR_CONSENSUS}
-
-# Define regions (same as before)
-REGIONS=("3:247434-251178")
 
 # Initialize or clear region-specific consensus FASTA files
 for REGION in "${REGIONS[@]}"; do
@@ -95,11 +54,11 @@ for REGION in "${REGIONS[@]}"; do
     > ${REGION_FASTA_FILE} # Clear the file if exists or create it if not
 done
 
-# Iterate over individual VCF files to append consensus sequences with sample names
-for VCF_FILE in ${VCF_DIR}/S*.vcf.gz; do
+# Iterate over VCF files that have 'sorted_md' in their names
+for VCF_FILE in ${VCF_DIR}/*sorted_md*.vcf.gz; do
     STRAIN_NAME=$(basename ${VCF_FILE} | cut -d'.' -f1)
 
-    # Index the VCF file if not already indexed
+    # Ensure the VCF file is indexed
     if [ ! -f "${VCF_FILE}.tbi" ]; then
         bcftools index ${VCF_FILE}
     fi
