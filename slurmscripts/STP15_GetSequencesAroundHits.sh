4e546d28c487a24ef6a11ff3bc67ada2659c7655
denizakdemir <deniz.akdemir.work@gmail.com>
2024-09-29 18:11:57 +0200

    STP15 changes

--- slurmscripts/STP15_GetSequencesAroundHits.sh
+++ slurmscripts/STP15_GetSequencesAroundHits.sh
@@ -17,13 +17,20 @@ module load BEDTools/2.28.0-GCC-8.2.0-2.31.1
 
 # Define variables
 REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_FILE="5_annotation/snpEff_snps/geno_filtered_snps.vcf"
+VCF_DIR="4_processing/GVCF"
 OUTPUT_DIR="extracted_sequences"
-mkdir -p ${OUTPUT_DIR}
 
+# Create different subfolders for bed, fasta, vcf, and consensus sequences
+BED_DIR="${OUTPUT_DIR}/bed_files"
+FASTA_DIR="${OUTPUT_DIR}/fasta_files"
+VCF_OUTPUT_DIR="${OUTPUT_DIR}/vcf_files"
+CONSENSUS_DIR="${OUTPUT_DIR}/consensus_sequences"
+
+# Create the output directories
+mkdir -p ${BED_DIR} ${FASTA_DIR} ${VCF_OUTPUT_DIR} ${CONSENSUS_DIR}
 
 # Define regions to extract - 500bp flanking regions
-REGIONS=("3:247434-251178")  # "7:2122486-2123486" "2:3164088-3165088")
+REGIONS=("3:247434-251178")
 
 # Extract SNP positions and create BED files
 for REGION in "${REGIONS[@]}"; do
@@ -35,24 +42,18 @@ for REGION in "${REGIONS[@]}"; do
     END=${POS[1]}
     
     # Create a bed file for each region
-    echo -e "${CHROM}\t${START}\t${END}" > "${OUTPUT_DIR}/${CHROM}_${START}_${END}.bed"
+    echo -e "${CHROM}\t${START}\t${END}" > "${BED_DIR}/${CHROM}_${START}_${END}.bed"
     
     # Use bedtools to extract sequences
-    bedtools getfasta -fi "${REFERENCE_GENOME}" -bed "${OUTPUT_DIR}/${CHROM}_${START}_${END}.bed" -fo "${OUTPUT_DIR}/${CHROM}_${START}_${END}.fasta"
+    bedtools getfasta -fi "${REFERENCE_GENOME}" -bed "${BED_DIR}/${CHROM}_${START}_${END}.bed" -fo "${FASTA_DIR}/${CHROM}_${START}_${END}.fasta"
 done
 
 echo "Reference sequence extraction completed."
-# Define variables
-REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_DIR="4_processing/GVCF"
-OUTPUT_DIR="extracted_sequences"
-mkdir -p ${OUTPUT_DIR}
 
-# Define SNP positions
+# Define SNP positions for genotypes
 SNP_POSITIONS=("3:247434-251178")
 
-
-# Iterate over individual VCF files
+# Iterate over individual VCF files to extract genotypes
 for VCF_FILE in ${VCF_DIR}/S*.vcf.gz; do
     # Extract the strain name from the VCF filename
     STRAIN_NAME=$(basename ${VCF_FILE} | cut -d'.' -f1)
@@ -65,7 +66,7 @@ for VCF_FILE in ${VCF_DIR}/S*.vcf.gz; do
     # Extract genotypes for each SNP position for the current strain
     for SNP_POS in "${SNP_POSITIONS[@]}"; do
         # Define output file name based on strain and SNP position
-        OUTPUT_FILE="${OUTPUT_DIR}/${STRAIN_NAME}_$(echo ${SNP_POS} | tr ':' '_')_genotypes.txt"
+        OUTPUT_FILE="${VCF_OUTPUT_DIR}/${STRAIN_NAME}_$(echo ${SNP_POS} | tr ':' '_')_genotypes.txt"
         
         # Use bcftools query to extract genotypes
         bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n' -r ${SNP_POS} ${VCF_FILE} > ${OUTPUT_FILE}
@@ -74,26 +75,15 @@ done
 
 echo "Genotype extraction for individual strains completed."
 
-# Define directories and files
-REFERENCE_GENOME="0_index/referenceIPO323/Zymoseptoria_tritici.MG2.dna.toplevel.fa"
-VCF_DIR="4_processing/GVCF"
-OUTPUT_DIR="extracted_sequences"
-OUTPUT_DIR_CONSENSUS="${OUTPUT_DIR}/consensus_sequences"
-mkdir -p ${OUTPUT_DIR}
-mkdir -p ${OUTPUT_DIR_CONSENSUS}
-
-# Define regions (same as before)
-REGIONS=("3:249890-250273")
-
-# Initialize or clear region-specific consensus FASTA files
+# Consensus sequence generation and organization
 for REGION in "${REGIONS[@]}"; do
     IFS=':' read -ra ADDR <<< "$REGION"
     CHROM=${ADDR[0]}
     IFS='-' read -ra POS <<< "${ADDR[1]}"
     START=${POS[0]}
     END=${POS[1]}
-    REGION_FASTA_FILE="${OUTPUT_DIR_CONSENSUS}/consensus_${CHROM}_${START}_${END}.fasta"
-    > ${REGION_FASTA_FILE} # Clear the file if exists or create it if not
+    REGION_FASTA_FILE="${CONSENSUS_DIR}/consensus_${CHROM}_${START}_${END}.fasta"
+    > ${REGION_FASTA_FILE}  # Clear or initialize the consensus FASTA file
 done
 
 # Iterate over individual VCF files to append consensus sequences with sample names
@@ -113,9 +103,9 @@ for VCF_FILE in ${VCF_DIR}/S*.vcf.gz; do
         START=${POS[0]}
         END=${POS[1]}
         REGION_STR="${CHROM}:${START}-${END}"
-        REGION_FILE="${OUTPUT_DIR}/${CHROM}_${START}_${END}.fasta"
-        TEMP_VCF="${OUTPUT_DIR}/${STRAIN_NAME}_${CHROM}_${START}_${END}.vcf"
-        REGION_FASTA_FILE="${OUTPUT_DIR_CONSENSUS}/consensus_${CHROM}_${START}_${END}.fasta"
+        REGION_FILE="${FASTA_DIR}/${CHROM}_${START}_${END}.fasta"
+        TEMP_VCF="${VCF_OUTPUT_DIR}/${STRAIN_NAME}_${CHROM}_${START}_${END}.vcf"
+        REGION_FASTA_FILE="${CONSENSUS_DIR}/consensus_${CHROM}_${START}_${END}.fasta"
 
         # Filter VCF for the specific region
         bcftools view -Oz -o ${TEMP_VCF}.gz -r ${REGION_STR} ${VCF_FILE}
